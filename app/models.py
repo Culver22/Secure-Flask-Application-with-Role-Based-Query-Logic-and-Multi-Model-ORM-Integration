24aac098a8668e67477f8bc707899fba52956534
Culver22 <o.culver1@newcastle.ac.uk>
2025-11-09 14:39:54 +0000

    Created the Post model, for user posts, includes the columns: id, title, content, time of creation and author id. I also included a repr method that returns a readable string output of the post

--- app/models.py
+++ app/models.py
@@ -21,6 +21,20 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return f"<User {self.username} ({self.role})>"
 
+class Post(db.Model):
+    __tablename__ = 'post'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    # foreign key is the user id
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"<Post {self.id} '{self.title}'>"
+
 
 def seed_data():
 
