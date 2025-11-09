a543393406ba70ad5b13429e8eddb6de2fe85d2e
Culver22 <o.culver1@newcastle.ac.uk>
2025-11-09 14:33:14 +0000

    added a repr class to get a user's username and role in a readable string format

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ from . import db
 class User(db.Model, UserMixin):
     __tablename__ = 'user'
 
+    # establish columns
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -16,6 +17,9 @@ class User(db.Model, UserMixin):
 
     # User should be a one-to-many relationship, and if deleted so should all their posts
     posts = db.relationship("Post", backref="author", lazy=True, cascade="all, delete-orphan")
+
+    def __repr__(self):
+        return f"<User {self.username} ({self.role})>"
 
 
 def seed_data():
