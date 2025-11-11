ebccc85a881d98047ffe6eadb2cada976509f62d
Culver22 <o.culver1@newcastle.ac.uk>
2025-11-11 15:34:18 +0000

    Added UTC formatting for logs, also had an issue with circular imports, currently importing later in the file however might change this

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_login import LoginManager
-from .models import User
+import logging
+import time
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -17,10 +18,27 @@ def create_app():
     # route name for login
     login_manager.login_view = 'main.login'  # means @login_required will redirect to main.login
 
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        fmt='%(asctime)s | %(name)s | %(levelname)s | %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    logging.Formatter.converter = time.gmtime  # UTC timestamps for log entries
+    handler.setFormatter(formatter)
+    app.logger.setLevel(logging.INFO)
+
+    # prevent duplicate logs when debug auto-reloads
+    if not app.logger.handlers:
+        app.logger.addHandler(handler)
+
+    # import routes after extensions, avoiding circular imports
     from .routes import main
     app.register_blueprint(main)
 
     return app
+
+# import User after db is defined so models can import db without circles
+from .models import User
 
 # user loader used by flask-login
 @login_manager.user_loader
